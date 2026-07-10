482e79b98b38673c6dccbd6c5708c89f9b2243fb
YALLICO OWNER <yallico2024@gmail.com>
2026-07-09 21:10:45 -0500

    Actualizar ia-gemini.js

--- plugins/ia-gemini.js
+++ plugins/ia-gemini.js
@@ -1,15 +1,16 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, text }) => {
-  if (!text) return m.reply('Escribe algo para hablar con Gemini.')
-  await m.react('⏳')
+  if (!text) return m.reply('⛈️ *RAYO PREM IA* ➔ Escribe tu consulta para Gemini\n⚡ *Ejemplo:* .gemini explica los rayos') // Cambiado
+  await m.react('🌩️') // Cambiado
   let key = Buffer.from('c2FzdWtl', 'base64').toString('utf-8')
   let res = await fetch(`https://api.evogb.org/ai/gemini?text=${encodeURIComponent(text)}&prompt=V&key=${key}`)
   let json = await res.json()
   if (json.status) {
-    await m.react('✅')
-    m.reply(json.result)
+    await m.react('⚡') // Cambiado
+    m.reply(`⛈️ *RAYO PREM GEMINI* 🌙\n\n${json.result}\n\n⚡ *Team Nightwish*`) // Cambiado
   } else {
     await m.react('❌')
+    m.reply('⛈️ *RAYO PREM ERROR* ➔ Gemini no respondió') // Cambiado
   }
 }
 handler.help = ['gemini <texto>']
