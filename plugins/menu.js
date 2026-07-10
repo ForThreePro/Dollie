8fba324399aa3b976e5cdefca678a80b24bd1870
YALLICO OWNER <yallico2024@gmail.com>
2026-07-09 21:06:17 -0500

    Actualizar menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -2,42 +2,42 @@ import { join } from 'path'
 import { readFileSync } from 'fs'
 
 let handler = async (m, { conn, usedPrefix }) => {
-  let taguser = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
-  
+  let taguser = m.mentionedJid && m.mentionedJid[0]? m.mentionedJid[0] : m.quoted? m.quoted.sender : m.sender
+
   const img = readFileSync(join(process.cwd(), 'storage', 'img', 'rayo.jpg'))
-  
-  let menuText = `🛸 *[ BOX BOT MD ]* 🌌\n\n`
+
+  let menuText = `⛈️ *[ RAYO PREM BOT ]* 🌙\n\n` // Cambiado
   menuText += `👤 *Usuario:* @${taguser.split('@')[0]}\n`
-  menuText += `⚙️ *Prefijo:* [ ${usedPrefix} ]\n\n`
-  
-  let help = Object.values(global.plugins).filter(p => p.help && !p.disabled)
+  menuText += `⚡ *Prefijo:* [ ${usedPrefix} ]\n\n` // Cambiado emoji
+
+  let help = Object.values(global.plugins).filter(p => p.help &&!p.disabled)
   let groups = {}
-  
+
   for (let plugin of help) {
-    let category = plugin.tags ? plugin.tags[0] : 'otros'
+    let category = plugin.tags? plugin.tags[0] : 'otros'
     if (!groups[category]) groups[category] = []
-    
+
     if (Array.isArray(plugin.help)) {
       groups[category].push(...plugin.help)
     } else {
       groups[category].push(plugin.help)
     }
   }
-  
+
   for (let category in groups) {
-    menuText += `*🛸 [ ${category.toUpperCase()} ] 🛸*\n`
+    menuText += `*⚡ [ ${category.toUpperCase()} ] ⚡*\n` // Cambiado
     for (let cmd of groups[category]) {
-      menuText += `│ 🌀 ${usedPrefix}${cmd}\n`
+      menuText += `│ 🌙 ${usedPrefix}${cmd}\n` // Cambiado emoji
     }
     menuText += `*───────────────────*\n\n`
   }
-  
-  menuText += `⚙️ *Box Bot MD • Sistema Automatizado* 🌀`
 
-  await conn.sendMessage(m.chat, { 
-    image: img, 
-    caption: menuText, 
-    mentions: [taguser] 
+  menuText += `⚡ *Team Nightwish • Whois Yallico* 🌙` // Cambiado
+
+  await conn.sendMessage(m.chat, {
+    image: img,
+    caption: menuText,
+    mentions: [taguser]
   }, { quoted: m })
 }
 handler.command = /^(menu|help|menú)$/i
