74a97bc0946df07e46adb861d9320362b55a8751
YALLICO OWNER <yallico2024@gmail.com>
2026-07-11 10:24:48 -0500

    Actualizar owner-reset.js

--- plugins/owner-reset.js
+++ plugins/owner-reset.js
@@ -1,12 +1,23 @@
-let handler = async (m, { conn }) => {
-    await m.react('⚡') // Cambiado
-    await m.reply('⛈️ *[ RAYO PREM BOT ]* 🌙\n\n> *Reiniciando sistema con el trueno... Por favor espere*') // Cambiado
-    process.send('reset')
+import { spawn } from 'child_process'
+
+var handler = async (m, { conn, isROwner, text }) => {
+
+if (!process.send) throw 'Dont: node mini.js\nDo: node index.js'
+if (conn.user.jid == conn.user.jid) {
+await conn.reply(m.chat, `╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+│ 🔄 *REINICIANDO BOT*
+│
+│ ⚡ *Estado:* Reiniciando sistemas
+│ 🌙 *El trueno volverá en segundos*
+╰─────────────────❒`, m)
+process.send('reset')
+} else throw 'eh'
+
 }
 
-handler.help = ['reset']
+handler.help = ['restart']
 handler.tags = ['owner']
-handler.command = ['reset']
+handler.command = ['restart','reiniciar'] 
 handler.rowner = true
 
 export default handler
