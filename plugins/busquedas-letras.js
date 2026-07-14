d71a7b2fe664cdb5488d625ee5fa639bcd0c90a4
YALLICO OWNER <yallico2024@gmail.com>
2026-07-14 11:29:02 -0500

    Actualizar busquedas-letras.js

--- plugins/busquedas-letras.js
+++ plugins/busquedas-letras.js
@@ -1,44 +1,44 @@
 import axios from 'axios'
 
 let handler = async (m, { conn, text }) => {
-    if (!text) return m.reply(`╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+    if (!text) return m.reply(`╭─❒ *『 𝗥𝗜𝗖𝗞𝗬 𝗕𝗢𝗧 𝗣𝗥𝗘𝗠 』* ❒
 │ 🎵 *BUSCADOR DE LETRAS*
 │
-│ 🌙 *¿Qué canción buscas?*
-│ ⚡ *Uso:*.letra Nightwish
+│ 👾 *¿Qué canción buscas?*
+│ ⚡ *Uso:*.letra Believer
 ╰─────────────────❒`)
 
     await m.react('🎵')
     try {
         let { data } = await axios.get(`https://api.delirius.store/search/lyrics?query=${encodeURIComponent(text)}`)
         let res = data.data
-        if (!res) return m.reply(`╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+        if (!res) return m.reply(`╭─❒ *『 𝗥𝗜𝗖𝗞𝗬 𝗕𝗢𝗧 𝗣𝗥𝗘𝗠 』* ❒
 │ ❌ *SIN RESULTADOS*
 │
 │ ⚡ *No encontré la letra de:* ${text}
 ╰─────────────────❒`)
 
-        let txt = `╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+        let txt = `╭─❒ *『 𝗥𝗜𝗖𝗞𝗬 𝗕𝗢𝗧 𝗣𝗥𝗘𝗠 』* ❒
 │ 🎵 *LETRA ENCONTRADA*
 │
 │ ⚡ *Título:* ${res.title}
 │ 👤 *Artista:* ${res.artists}
 │ ⏳ *Duración:* ${res.duration || 'N/A'}
 ╰─────────────────❒\n\n`
-        txt += `╭─── 🌙 𝗟𝗘𝗧𝗥𝗔 𝗡𝗢𝗖𝗧𝗨𝗥𝗡𝗔 ───╮\n`
+        txt += `╭─── 👾 𝗟𝗘𝗧𝗥𝗔 ───╮\n`
         txt += `${res.lyrics}\n`
-        txt += `╰─────────────────────────╯\n\n`
-        txt += `> *“La música resuena en la tormenta”* ⚡\n> *© Team Nightwish*`
+        txt += `╰─────────────────╯\n\n`
+        txt += `> *“La música en tu idioma”* ⚡\n> *© Ricky Bot Prem*`
 
         m.reply(txt)
         await m.react('✅')
     } catch { 
         await m.react('❌')
-        m.reply(`╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+        m.reply(`╭─❒ *『 𝗥𝗜𝗖𝗞𝗬 𝗕𝗢𝗧 𝗣𝗥𝗘𝗠 』* ❒
 │ ⛈️ *ERROR*
 │
 │ ⚡ *Falló al buscar la letra*
-│ 🌙 *Intenta de nuevo*
+│ 👾 *Intenta de nuevo*
 ╰─────────────────❒`)
     }
 }
