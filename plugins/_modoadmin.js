91a260798ba0dbd4809ceee7c61de70deb5dbb47
YALLICO OWNER <yallico2024@gmail.com>
2026-07-14 11:23:56 -0500

    Actualizar _modoadmin.js

--- plugins/_modoadmin.js
+++ plugins/_modoadmin.js
@@ -1,6 +1,6 @@
 const handler = async (m, { conn, args, isAdmin, isOwner }) => {
     // Validación de permisos para el comando
-    if (!isAdmin &&!isOwner) throw `╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+    if (!isAdmin &&!isOwner) throw `╭─❒ *『 𝗥𝗜𝗖𝗞𝗬 𝗕𝗢𝗧 𝗣𝗥𝗘𝗠 』* ❒
 │ 🛡️ *ACCESO DENEGADO*
 │ Solo *ADMINS* pueden usar esto
 ╰─────────────────❒`
@@ -10,33 +10,33 @@ const handler = async (m, { conn, args, isAdmin, isOwner }) => {
 
     if (/on/i.test(args[0])) {
         chat.modoadmin = true
-        await conn.reply(m.chat, `╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+        await conn.reply(m.chat, `╭─❒ *『 𝗥𝗜𝗖𝗞𝗬 𝗕𝗢𝗧 𝗣𝗥𝗘𝗠 』* ❒
 │ ⚡ *MODO ADMIN ACTIVADO*
 │
-│ 🌙 *Estado:* ENCENDIDO
+│ 👾 *Estado:* ENCENDIDO
 │ 🛡️ *Restricción:* Solo Admins
 │ ⛈️ *Los comandos están bloqueados*
 │
-│ > *“El trueno vigila el grupo”*
+│ > *“Ricky controla el grupo”*
 ╰─────────────────❒`, m)
     } else if (/off/i.test(args[0])) {
         chat.modoadmin = false
-        await conn.reply(m.chat, `╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+        await conn.reply(m.chat, `╭─❒ *『 𝗥𝗜𝗖𝗞𝗬 𝗕𝗢𝗧 𝗣𝗥𝗘𝗠 』* ❒
 │ ❌ *MODO ADMIN DESACTIVADO*
 │
-│ 🌙 *Estado:* APAGADO
+│ 👾 *Estado:* APAGADO
 │ ✅ *Todos pueden usar el bot*
 │ 🌩️ *Chat libre nuevamente*
 ╰─────────────────❒`, m)
     } else {
-        await conn.reply(m.chat, `╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+        await conn.reply(m.chat, `╭─❒ *『 𝗥𝗜𝗖𝗞𝗬 𝗕𝗢𝗧 𝗣𝗥𝗘𝗠 』* ❒
 │ 🌩️ *PANEL MODO ADMIN*
 │
 │ 📌 *Uso:*.modoadmin on /.modoadmin off
 │ ⚡ *Función:* Restringe comandos a admins
 │ 🛡️ *Protege:* Uso indebido del bot
 │
-│ 🌙 *Control total del grupo*
+│ 👾 *Control total del grupo*
 ╰─────────────────❒`, m)
     }
 }
@@ -57,12 +57,12 @@ handler.before = async function (m, { conn, isAdmin, isOwner, isROwner, isPrems 
         if (chat.modoadmin &&!isAdmin &&!isOwner &&!isROwner &&!isPrems) {
             // Si el usuario intenta usar un comando (empieza con prefijo), bloqueamos
             if (m.text.startsWith('.') || m.text.startsWith('/') || m.text.startsWith('#')) {
-                await conn.reply(m.chat, `╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+                await conn.reply(m.chat, `╭─❒ *『 𝗥𝗜𝗖𝗞𝗬 𝗕𝗢𝗧 𝗣𝗥𝗘𝗠 』* ❒
 │ ⛈️ *ACCESO BLOQUEADO*
 │
 │ ⚡ *Modo Admin:* ACTIVO
 │ ❌ *No tienes permiso*
-│ 🌙 *Solo admins pueden usar comandos*
+│ 👾 *Solo admins pueden usar comandos*
 ╰─────────────────❒`, m)
                 return false // Detiene la ejecución de otros plugins
             }
