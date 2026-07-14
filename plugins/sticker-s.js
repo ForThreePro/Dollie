916df067bf192acbf6ef6a08de807e22b1aaab9d
YALLICO OWNER <yallico2024@gmail.com>
2026-07-14 13:10:42 -0500

    Actualizar sticker-s.js

--- plugins/sticker-s.js
+++ plugins/sticker-s.js
@@ -1,22 +1,24 @@
 import { sticker } from '../lib/sticker.js'
 
+const MARCA = 'Ricky Bot Prem 👾'
+
 let handler = async (m, { conn, usedPrefix, command }) => {
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || q.mediaType || ''
-    if (!/webp|image|video/g.test(mime)) return m.reply(`╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
+    if (!/webp|image|video/g.test(mime)) return m.reply(`╭─❒ *『 ${MARCA} 』* ❒
 │ 🎭 *STICKER*
 │
 │ ⚠️ *Responde a una imagen, video o gif*
-│ 🌙 *Para convertirlo en sticker*
+│ 👾 *Para convertirlo en sticker*
 ╰─────────────────❒`)
-    
+
     let img = await q.download()
-    let stiker = await sticker(img, false, 'Team Nightwish', 'Whois Yallico')
-    
-    await conn.sendFile(m.chat, stiker, 'sticker.webp', `╭─❒ *『 𝗧𝗘𝗔𝗠 𝗡𝗜𝗚𝗛𝗧𝗪𝗜𝗦𝗛 』* ❒
-│ ⚡ *STICKER CREADO*
+    let stiker = await sticker(img, false, MARCA, MARCA)
+
+    await conn.sendFile(m.chat, stiker, 'sticker.webp', `╭─❒ *『 ${MARCA} 』* ❒
+│ 👾 *STICKER CREADO*
 │
-│ > *El trueno en forma de sticker*
+│ > *Ricky en forma de sticker*
 ╰─────────────────❒`, m)
 }
 
