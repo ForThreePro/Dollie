259ecb06a2b9ccc2ed6a8450b8ed05d5af672ee3
YALLICO OWNER <yallico2024@gmail.com>
2026-07-22 09:11:58 -0500

    Crear _love.js

---
+++ plugins/_love.js
@@ -0,0 +1,33 @@
+let handler = async (m, { conn, command }) => {
+  let who = m.mentionedJid && m.mentionedJid[0]
+        ? m.mentionedJid[0]
+          : m.quoted
+        ? m.quoted.sender
+          : m.sender;
+
+  let yo = m.sender
+  let nameYo = await conn.getName(yo);
+  let nameUser = await conn.getName(who);
+  let porcentaje = Math.floor(Math.random() * 101);
+
+  if(command == 'love'){
+    let frase = porcentaje < 30
+    ? '💔 *MEJOR AMIGOS*'
+      : porcentaje < 60
+    ? '😏 *HAY CHANCE*'
+      : porcentaje < 85
+    ? '💕 *SE VEN BIEN JUNTOS*'
+      : '💍 *CÁSENSE YA*'
+
+    await conn.sendMessage(m.chat, {
+      text: `🎀 *DOLLIE BOT LOVE SCANNER* 💘\n\n✨ *@${yo.split('@')[0]}* y *@${who.split('@')[0]}* *TIENEN* *${porcentaje}%* *DE COMPATIBILIDAD*\n${frase}\n🌸 *Dollie Love System*`,
+      mentions: [yo, who]
+    }, {quoted: m})
+  }
+}
+
+handler.help = ['love *@user*']
+handler.tags = ['love']
+handler.command = /^(love)$/i
+
+export default handler
