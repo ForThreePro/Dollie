2586bbeb50a7c73acfb469d1e76cede08bba0d25
YALLICO OWNER <yallico2024@gmail.com>
2026-07-21 20:06:32 -0500

    Actualizar sticker-wm.js

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -1,24 +1,24 @@
 import { addExif } from '../lib/sticker.js'
 
-const MARCA = 'Ricky Bot Prem 👾'
+const MARCA = '𝗗𝗢𝗟𝗟𝗜𝗘 𝗕𝗢𝗧 🎀'
 
 let handler = async (m, { conn, text }) => {
-  if (!m.quoted) return conn.reply(m.chat, `👾 *${MARCA}* ➔ Responde a un *sticker* para robarlo.`, m)
+  if (!m.quoted) return conn.reply(m.chat, `🎀 *${MARCA}* ➔ Responde a un *sticker* para robarlo.`, m)
   let stiker = false
   try {
     let [packname, ...author] = text.split('|')
     author = (author || []).join('|')
     let mime = m.quoted.mimetype || ''
-    if (!/webp/.test(mime)) return conn.reply(m.chat, `👾 *${MARCA}* ➔ Eso no es un *sticker*. Responde a un sticker.`, m)
+    if (!/webp/.test(mime)) return conn.reply(m.chat, `🎀 *${MARCA}* ➔ Eso no es un *sticker*. Responde a un sticker.`, m)
     let img = await m.quoted.download()
-    if (!img) return conn.reply(m.chat, `👾 *${MARCA}* ➔ No pude descargar el *sticker*.`, m)
-    stiker = await addExif(img, packname || MARCA, author || MARCA) // Marca por defecto
+    if (!img) return conn.reply(m.chat, `🎀 *${MARCA}* ➔ No pude descargar el *sticker*.`, m)
+    stiker = await addExif(img, packname || MARCA, author || '✨ Premium Edition ✨') // Marca por defecto
   } catch (e) {
     console.error(e)
     if (Buffer.isBuffer(e)) stiker = e
   } finally {
-    if (stiker) conn.sendFile(m.chat, stiker, 'RickyWM.webp', `👾 *${MARCA}* | Sticker reclamado`, m)
-    else return conn.reply(m.chat, `👾 *${MARCA}* ➔ Error al procesar el *sticker*.`, m)
+    if (stiker) conn.sendFile(m.chat, stiker, 'DollieWM.webp', `🎀 *${MARCA}* | Sticker reclamado 💫`, m)
+    else return conn.reply(m.chat, `🎀 *${MARCA}* ➔ Error al procesar el *sticker*.`, m)
   }
 }
 handler.help = ['wm <nombre>|<autor>']
