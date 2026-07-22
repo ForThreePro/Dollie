cc8e73fc4eb04bac6d4759c1d4187a6cfd28935e
YALLICO OWNER <yallico2024@gmail.com>
2026-07-21 20:05:54 -0500

    Actualizar sticker-s.js

--- plugins/sticker-s.js
+++ plugins/sticker-s.js
@@ -1,28 +1,28 @@
 import { sticker } from '../lib/sticker.js'
 
-const MARCA = 'Ricky Bot Prem 👾'
+const MARCA = '𝗗𝗢𝗟𝗟𝗜𝗘 𝗕𝗢𝗧 🎀'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || q.mediaType || ''
-    if (!/webp|image|video/g.test(mime)) return m.reply(`╭─❒ *『 ${MARCA} 』* ❒
-│ 🎭 *STICKER*
+    if (!/webp|image|video/g.test(mime)) return m.reply(`╭─🎀─❒ *『 ${MARCA} 』* ❒─🎀─╮
+│ ✨ *STICKER*
 │
 │ ⚠️ *Responde a una imagen, video o gif*
-│ 👾 *Para convertirlo en sticker*
-╰─────────────────❒`)
+│ 🎀 *Para convertirlo en sticker*
+╰─────────────────────────╯`)
 
     let img = await q.download()
-    let stiker = await sticker(img, false, MARCA, MARCA)
+    let stiker = await sticker(img, false, MARCA, '✨ Premium Edition ✨')
 
-    await conn.sendFile(m.chat, stiker, 'sticker.webp', `╭─❒ *『 ${MARCA} 』* ❒
-│ 👾 *STICKER CREADO*
+    await conn.sendFile(m.chat, stiker, 'DollieSticker.webp', `╭─🎀─❒ *『 ${MARCA} 』* ❒─🎀─╮
+│ 💫 *STICKER CREADO*
 │
-│ > *Ricky en forma de sticker*
-╰─────────────────❒`, m)
+│ > *Dollie en forma de sticker* 🎀
+╰─────────────────────────╯`, m)
 }
 
-handler.help = ['s']
+handler.help = ['s', 'sticker', 'stiker']
 handler.tags = ['sticker']
 handler.command = ['s', 'sticker', 'stiker']
 
